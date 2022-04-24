614d9cb1a84a1f390a88578d5d430d8e2be8c873
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-04-24 00:23:26 -0500

    Paso 3: TDD aplicando este metodo al proyecto

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,14 +1,16 @@
+const Spiderman = require('./../app/spiderman')
+
 describe("Unit Tests for Spiderman Class", () => {
     test('1) Create an spiderman object', () => {
         //Aqui escribimos el codigoque queremos usar tal cual
         //Quiero poder instanciar un objeto spiderman con esta informacion
-        const andrewGarfield = new Spiderman("Spiderman Sony", 32, "Andrew Garfield", 2, "Sony")
+        const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
         
         //Validamos que este codigo funcione de la forma adecuada
         expect(andrewGarfield.name).toBe("Spiderman Sony");
         expect(andrewGarfield.age).toBe(31);
         expect(andrewGarfield.actor).toBe("Andrew Garfield");
-        expect(andrewGarfield.movies).toBe(31);
+        expect(andrewGarfield.movies).toBe(2);
         expect(andrewGarfield.studio).toBe("Sony");
     });
   })
